ae6f1c53cf38f84ad051e04b556e240278b973d7
IWasAllen <91869534+IWasAllen@users.noreply.github.com>
2022-05-30 20:51:46 +0800

    Download This

---
+++ scenes.lua
@@ -0,0 +1,15 @@
+local M = {}
+M.path = nil
+
+function M:change(path)
+    if M.currentpath ~= nil then
+        package.loaded[M.currentpath] = nil
+        collectgarbage()
+    end
+
+    M.currentpath = M.path .. "/" .. path
+    print("A")
+    M.current = require(M.currentpath)
+end
+
+return M
