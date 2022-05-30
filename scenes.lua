962f324599487dc001b0ff63cbd9f135b6ed2d6b
IWasAllen <91869534+IWasAllen@users.noreply.github.com>
2022-05-30 21:21:20 +0800

    Download This

--- scenes.lua
+++ scenes.lua
@@ -3,6 +3,7 @@ M.path = nil
 
 function M:change(path)
     if M.currentpath ~= nil then
+        -- Unload the previous scene to save memory --
         package.loaded[M.currentpath] = nil
         collectgarbage()
     end
